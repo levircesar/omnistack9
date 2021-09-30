b2048d98c4dfaa2221a1275f6a45a45c65ccd3e2
levircesar <levirlemos@gmail.com>
2021-09-30 10:20:21 -0300

    update files to heroku

--- src/server.js
+++ src/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack.noc4w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect('mongodb://omnistack:omnistack@omnistack-shard-00-00.noc4w.mongodb.net:27017,omnistack-shard-00-01.noc4w.mongodb.net:27017,omnistack-shard-00-02.noc4w.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-wumwv5-shard-0&authSource=admin&retryWrites=true&w=majority',{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 /**
  * req.query = acessar query params  para filtros
  * req.params = acessar route params para edicao, delete
  * req.body = acessar corpo da requisicao para criacao, edicao
  */
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
